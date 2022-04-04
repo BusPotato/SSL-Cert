718bf7ed439ca701b11b834ada5371b8532e2fc5
BusPotato <69567266+BusPotato@users.noreply.github.com>
2022-04-04 14:42:58 +0800

    Update gencert.sh

--- gencert.sh
+++ gencert.sh
@@ -3,4 +3,4 @@
 rm -r output
 mkdir output
 
-openssl req -config cert_config.conf -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout output/cert.key -days 3650 -out output/cert.pem
+openssl req -config cert_config.conf -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout output/zgh.key -days 36500 -out output/zgh.pem
